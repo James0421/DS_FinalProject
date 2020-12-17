04e6fe60abb6e108a45d328c875ab4301181a1e4
James0421 <108306037@nccu.edu.tw>
2020-12-17 11:45:38 +0800

    first commit

---
+++ eclipse-workspace/FinalProject/src/ResultsPage.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class ResultsPage extends JFrame{
+	private static final int FRAME_WIDTH = 760;
+	private static final int FRAME_HEIGHT = 720;
+	public ResultsPage() {
+		setTitle("Results");
+		setSize(FRAME_WIDTH, FRAME_HEIGHT);
+	}
+}
---
+++ eclipse-workspace/FinalProject/src/SearechPageViewer.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+public class SearechPageViewer {
+	public static void main(String[] args) {
+		JFrame frame = new SearchPage();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+}
